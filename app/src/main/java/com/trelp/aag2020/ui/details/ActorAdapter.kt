c894149a6bfb86933185793c7c6456984c0f6b4c
Sergei Uzenev <ostoperla@gmail.com>
2020-12-07 18:32:27 +0300

    Implemented FragmentMovieDetails

---
+++ app/src/main/java/com/trelp/aag2020/ui/details/ActorAdapter.kt
@@ -0,0 +1,41 @@
+package com.trelp.aag2020.ui.details
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.trelp.aag2020.data.Actor
+import com.trelp.aag2020.databinding.ItemActorBinding
+import com.trelp.aag2020.ui.common.utils.inflater
+
+class ActorAdapter : RecyclerView.Adapter<ActorAdapter.ActorHolder>() {
+
+    private var data = listOf<Actor>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorHolder {
+        return ActorHolder(
+            ItemActorBinding.inflate(parent.inflater, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ActorHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount() = data.size
+
+    fun setupData(newData: List<Actor>) {
+        data = newData
+        notifyDataSetChanged()
+    }
+
+    class ActorHolder(
+        private val binding: ItemActorBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(actor: Actor) {
+            with(binding) {
+                imageActorPhoto.setImageResource(actor.photo)
+                textActorName.text = actor.name
+            }
+        }
+    }
+}
--- app/src/main/java/com/trelp/aag2020/ui/details/FragmentMovieDetails.kt
+++ app/src/main/java/com/trelp/aag2020/ui/details/FragmentMovieDetails.kt
@@ -1,9 +1,75 @@
 package com.trelp.aag2020.ui.details
 
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.Px
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.trelp.aag2020.R
+import com.trelp.aag2020.data.MovieDetailsDataSource
+import com.trelp.aag2020.databinding.FragmentMovieDetailsBinding
 import com.trelp.aag2020.ui.common.BaseFragment
 
 class FragmentMovieDetails : BaseFragment(R.layout.fragment_movie_details) {
+
+    private val binding
+        get() = viewBinding!! as FragmentMovieDetailsBinding
+
+    private var movieId: Int = 0
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        arguments?.let {
+            movieId = it.getInt(ARG_MOVIE_ID, 0)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewBinding = FragmentMovieDetailsBinding.bind(view)
+
+        val movieDetails = MovieDetailsDataSource().getMovieDetails(movieId)
+
+        with(binding) {
+            imageMovieLogo.setImageResource(movieDetails.poster)
+            textMovieRatingSystem.text = movieDetails.ageLimit
+            textMovieName.text = movieDetails.title
+            textMovieTags.text = movieDetails.tags
+            textMovieReviews.text = resources.getQuantityString(
+                R.plurals.movie_reviews,
+                movieDetails.reviewCount,
+                movieDetails.reviewCount
+            )
+            textMovieStorylineContent.text = movieDetails.overview
+        }
+
+        with(binding.listActor) {
+            adapter = ActorAdapter().also { it.setupData(movieDetails.actors) }
+            addItemDecoration(
+                DividerItemDecoration(context, RecyclerView.HORIZONTAL).also {
+                    it.setSpaceDrawable(
+                        resources.getDimensionPixelOffset(R.dimen.item_actor_offset),
+                        0
+                    )
+                }
+            )
+            layoutManager = LinearLayoutManager(view.context, RecyclerView.HORIZONTAL, false)
+        }
+    }
+
+    private fun DividerItemDecoration.setSpaceDrawable(@Px width: Int, @Px height: Int) {
+        setDrawable(
+            object : ColorDrawable() {
+                override fun getIntrinsicWidth() = width
+                override fun getIntrinsicHeight() = height
+            }
+        )
+    }
 
     companion object {
         const val ARG_MOVIE_ID = "arg_movie_id"
