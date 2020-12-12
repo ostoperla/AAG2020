8ce3f2a9b8c617ca70b62542e6d70fe3ac464509
Sergei Uzenev <ostoperla@gmail.com>
2020-12-12 19:21:21 +0300

    Fixed nav logic

--- app/src/main/java/com/trelp/aag2020/ui/MainActivity.kt
+++ app/src/main/java/com/trelp/aag2020/ui/MainActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.trelp.aag2020.R
+import com.trelp.aag2020.ui.details.FragmentMovieDetails
 import com.trelp.aag2020.ui.movies.FragmentMoviesList
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity_main),
+    FragmentMoviesList.OnItemClickListener,
+    FragmentMovieDetails.OnBackButtonClick {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +26,25 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 add<FragmentMoviesList>(R.id.fragment_container)
             }
         }
+    }
+
+    private fun navigateToMovieDetails() {
+        supportFragmentManager.commit {
+            addToBackStack(null)
+            setReorderingAllowed(true)
+            replace<FragmentMovieDetails>(R.id.fragment_container)
+        }
+    }
+
+    private fun backToPreviousFragment() {
+        supportFragmentManager.popBackStack()
+    }
+
+    override fun onItemClick() {
+        navigateToMovieDetails()
+    }
+
+    override fun onBackButtonClick() {
+        backToPreviousFragment()
     }
 }
--- app/src/main/java/com/trelp/aag2020/ui/details/FragmentMovieDetails.kt
+++ app/src/main/java/com/trelp/aag2020/ui/details/FragmentMovieDetails.kt
@@ -1,6 +1,42 @@
 package com.trelp.aag2020.ui.details
 
+import android.content.Context
+import android.os.Bundle
+import android.view.View
 import com.trelp.aag2020.R
+import com.trelp.aag2020.databinding.FragmentMovieDetailsBinding
 import com.trelp.aag2020.ui.common.BaseFragment
 
-class FragmentMovieDetails : BaseFragment(R.layout.fragment_movie_details)
+class FragmentMovieDetails : BaseFragment(R.layout.fragment_movie_details) {
+
+    private val binding
+        get() = viewBinding!! as FragmentMovieDetailsBinding
+
+    private var backButtonClickListener: OnBackButtonClick? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        backButtonClickListener =
+            if (context is OnBackButtonClick) context
+            else throw ClassCastException("${context.javaClass} must implement OnBackButtonClick")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewBinding = FragmentMovieDetailsBinding.bind(view)
+
+        binding.textMovieBack.setOnClickListener { backButtonClickListener?.onBackButtonClick() }
+    }
+
+    override fun onDetach() {
+        backButtonClickListener = null
+
+        super.onDetach()
+    }
+
+    interface OnBackButtonClick {
+        fun onBackButtonClick()
+    }
+}
