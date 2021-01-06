b7d81884934266e0ac3ff76e3670df764d3e3638
Sergei Uzenev <ostoperla@gmail.com>
2021-01-06 17:05:33 +0300

    Implemented Tmdb API

---
+++ app/src/main/java/com/trelp/aag2020/data/network/TmdbAPI.kt
@@ -0,0 +1,36 @@
+package com.trelp.aag2020.data.network
+
+import com.trelp.aag2020.data.model.CastResponse
+import com.trelp.aag2020.data.model.ConfigurationResponse
+import com.trelp.aag2020.data.model.MovieDetailsResponse
+import com.trelp.aag2020.data.model.MoviesResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface TmdbAPI {
+
+    @GET("configuration")
+    suspend fun getConfiguration(): ConfigurationResponse
+
+    @GET("movie/now_playing")
+    suspend fun getNowPlaying(): MoviesResponse
+
+    @GET("movie/popular")
+    suspend fun getPopular(): MoviesResponse
+
+    @GET("movie/top_rated")
+    suspend fun getTopRated(): MoviesResponse
+
+    @GET("movie/upcoming")
+    suspend fun getUpcoming(): MoviesResponse
+
+    @GET("movie/{movie_id}")
+    suspend fun getDetails(
+        @Path("movie_id") movieId: Int
+    ): MovieDetailsResponse
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun getMovieCredits(
+        @Path("movie_id") movieId: Int
+    ): CastResponse
+}
